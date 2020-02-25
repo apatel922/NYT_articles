6dde863fa48fb60fdc1f2e0463a33fdaf33e1950
Christian Balentine <christianbalentine47@gmail.com>
2020-02-24 21:19:34 -0500

    JS v2

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.4.1.js"
-			  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-			  crossorigin="anonymous"></script>
-
-
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIKey;
 var APIKey = "LhGQy3wkxj7s4h6ipUMbNy1R8s8GG9N1";
 
